17da2291559e0c4d0e468cd9f35bbe2fdd4f1101
Pradeep Yadav <pradpyadav@gmail.com>
2016-11-15 20:11:59 -0600

    fixed message and showing sales data message

--- Demo1/src/DemoMain.java
+++ Demo1/src/DemoMain.java
@@ -10,8 +10,8 @@ public class DemoMain {
 	
 	public static void method1(){
 		for(int i=0;i<5;i++){
-			System.out.println("Printing "+i+" time.");
-			System.out.println("This is an extra line added");
+			System.out.println("Printing and fixing "+i+" time.");
+			System.out.println("This is an showing Sales Data ");
 			
 			
 		}
